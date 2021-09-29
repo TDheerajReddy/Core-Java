5a3165f6956e02af6ee4fe5f5885625f653d6a70
T.Dheeraj Reddy <74751453+TDheerajReddy@users.noreply.github.com>
2021-09-29 12:51:52 +0530

    Package
    Package

---
+++ Java/interpack/inter.java
@@ -0,0 +1,8 @@
+//Programs using on Package using Intereface
+package interpack;				//compile only
+public interface inter			//javac inter.java
+{
+	void getLength()throws Exception;
+	void getCompare()throws Exception;
+	void getChar()throws Exception;
+}
