2ce70adfbe66b49c1e175a96b48db80c4982282b
T.Dheeraj Reddy <74751453+TDheerajReddy@users.noreply.github.com>
2021-09-29 12:59:57 +0530

    Num & String
    Number & String.

---
+++ Java/third/sample1.java
@@ -0,0 +1,15 @@
+package third;
+public class sample1
+{
+	public void numdisp(int a,int b)
+	{
+		System.out.println("The value of a is: "+a);		
+		System.out.println("The value of b is: "+b);
+	}
+	public String strdisp(String s)
+	{
+		//String s="WELCOME";
+		//System.out.println("The Entered String is: "+s);
+		return "Hai !!! "+s+" Welcome to Package";
+	}
+};
