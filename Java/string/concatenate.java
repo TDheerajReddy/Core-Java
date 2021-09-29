a4164229959c84dce269f9e9235432dbdd1bebd3
T.Dheeraj Reddy <74751453+TDheerajReddy@users.noreply.github.com>
2021-09-29 12:58:44 +0530

    String
    String

---
+++ Java/string/concatenate.java
@@ -0,0 +1,8 @@
+package string;
+public class concatenate
+{
+	public void getconcat(String s,String s1)
+	{
+		System.out.println("Concatenation of Two Strings: "+s.concat(s1));		//"concat" is PreDefined method		print s & s1
+	}
+}
---
+++ Java/string/len.java
@@ -0,0 +1,9 @@
+package string;
+public class len
+{
+	public void getlength(String s)
+	{
+		int n=s.length();						//"length" is PreDefined method
+		System.out.println("Length: "+n);
+	}
+}
